aff9ce096a6d97e0388319e9487729f1c9901402
FEDAlexandra <oleksandrafedotova@gmail.com>
2021-11-21 17:18:28 +0200

    init create app

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+const Todo = (props) => {
+    return React.createElement('div',{},[
+    React.createElement('p',{},props.title),
+    React.createElement('p',{},props.date),
+    ])
+};
+
+
+
+
+const App = () => {
+    return React.createElement(
+     'div',
+     {id:'parent'},
+    React.createElement('div',{},[
+    React.createElement('h3',{}, 'Your todos'), 
+    React.createElement(Todo,{title:'Learn react',date:'01-01-2022'}),
+    React.createElement(Todo,{title:'Learn redux',date:'01-01-2022'}),
+    React.createElement(Todo,{title:'Learn angular',date:'01-01-2022'}),
+    ]),
+    );
+ };
+ ReactDOM.render(React.createElement(App),
+ document.getElementById('root'),
+);
